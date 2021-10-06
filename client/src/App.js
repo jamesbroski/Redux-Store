11a51f83c4258d54e7d36b61873ed13d7a705583
James Broski <james.broski.5@gmail.com>
2021-10-05 22:32:17 -0600

    refactored with redux, functional fixes

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store={store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -43,7 +43,7 @@ function App() {
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
